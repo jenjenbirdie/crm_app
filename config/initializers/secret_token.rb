4bccd9f4d47570cbeb54b2e7e71735a197486fae
jenjenbirdie <jennifer.birdie@gmail.com>
2011-07-16 23:33:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmApp::Application.config.secret_token = '26397260740d256c5c0afe0e5ca7738ba3c84f6b2e0f470768af7d2c7a9bebd54f393779f7fca1df342f19683b7f9f82e6ea1dbe1bad9be94c104875f6733dac'
