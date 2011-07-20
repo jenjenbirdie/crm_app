9512c19f57438b8498fd85cfd30ea210f2e1e67e
jenjenbirdie <jennifer.birdie@gmail.com>
2011-07-20 13:08:16 -0700

    Done with company form

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,9 @@
 class Company < ActiveRecord::Base
 	validates :companyName, :customerType, :industry, :presence => true
 	validates :companyName, :uniqueness => true
+	
+	COMPANY_TYPES = ["Parent", "Child", "Sole"]
+	CUSTOMER_TYPES = ["Partner", "Competitor", "Former Customer", "Vendor", "Reseller"]
+	INDUSTRY_TYPES = ["Software", "Manufacturing", "Finance", "Healthcare", "Others"]
+	VIP_TYPES = ["Low", "Medium", "High"]
 end
