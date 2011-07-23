fd4a47d752aa676c07ad1c21222e4e7e8e8f2fa4
jenjenbirdie <jennifer.birdie@gmail.com>
2011-07-22 20:03:37 -0700

    Done with change default page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ CrmApp::Application.routes.draw do
   resources :individuals
 
   resources :companies
+  
+  get "companies/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,6 +55,8 @@ CrmApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => 'companies#index'
 
   # See how all your routes lay out with "rake routes"
 
